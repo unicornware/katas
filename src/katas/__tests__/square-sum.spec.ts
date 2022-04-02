d6fcc81960dddf6575f059dfafa33bcee19f6043
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-02 18:44:43 -0400

    feat(katas): `squareSum`
    - https://codewars.com/kata/515e271a311df0350d00000f

---
+++ src/katas/__tests__/square-sum.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * @file Unit Tests - squareSum
+ * @module katas/tests/unit/squareSum
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../square-sum'
+
+describe('unit:katas/squareSum', () => {
+  interface Case extends TestcaseFn<typeof testSubject> {}
+
+  const cases: Case[] = [
+    { expected: 0, parameters: [[]] },
+    { expected: 5, parameters: [[1, 2]] },
+    { expected: 9, parameters: [[1, 2, 2]] },
+    { expected: 50, parameters: [[0, 3, 4, 5]] }
+  ]
+
+  cases.forEach(({ expected, parameters }) => {
+    it(`should return ${expected} given ${pf(parameters)}`, () => {
+      expect(testSubject(...parameters)).to.equal(expected)
+    })
+  })
+})
---
+++ src/katas/square-sum.ts
@@ -0,0 +1,20 @@
+/**
+ * @file Katas - squareSum
+ * @module katas/squareSum
+ */
+
+/**
+ * Given an array of numbers, `arr`, the functions squares each number in `arr`
+ * and returns the sum.
+ *
+ * @see https://codewars.com/kata/515e271a311df0350d00000f
+ *
+ * @example squareSum([1, 2, 2]) => 9
+ * @example squareSum([]) => 0
+ *
+ * @param {number[]} arr - Integer array
+ * @return {number} Sum of squares in `arr`
+ */
+const squareSum = (arr: number[]): number => arr.reduce((a, n) => a + n ** 2, 0)
+
+export default squareSum
