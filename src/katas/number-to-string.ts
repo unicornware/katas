e45f49d331192580c376b7ef66784578bb3d8a48
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-02 18:21:24 -0400

    feat(katas): `numberToString`
    - https://codewars.com/kata/5265326f5fda8eb1160004c8

---
+++ src/katas/number-to-string.ts
@@ -0,0 +1,19 @@
+/**
+ * @file Katas - numberToString
+ * @module katas/numberToString
+ */
+
+/**
+ * Given a number, `num`, the function returns the number as a string.
+ *
+ * @see https://codewars.com/kata/5265326f5fda8eb1160004c8
+ *
+ * @example numberToString(123) => '123'
+ * @example numberToString(999) => '999'
+ *
+ * @param {number} num - Number to stringify
+ * @return {string} Stringified number
+ */
+const numberToString = (num: number): string => `${num}`
+
+export default numberToString
