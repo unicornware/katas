e6abaa317aeb055727f649d98e19d69e8fccf623
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-04 13:25:25 -0400

    feat(katas): `countSmileys`
    - https://codewars.com/kata/583203e6eb35d7980400002a

---
+++ src/katas/__tests__/count-smileys.spec.ts
@@ -0,0 +1,28 @@
+/**
+ * @file Unit Tests - countSmileys
+ * @module katas/tests/unit/countSmileys
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../count-smileys'
+
+describe('unit:katas/countSmileys', () => {
+  interface Case extends TestcaseFn<typeof testSubject> {}
+
+  const cases: Case[] = [
+    { expected: 0, parameters: [[]] },
+    { expected: 4, parameters: [[':D', ':~)', ';~D', ':)']] },
+    { expected: 2, parameters: [[':)', ':(', ':D', ':O', ':;']] },
+    { expected: 1, parameters: [[';]', ':[', ';*', ':$', ';-D']] },
+    {
+      expected: 2,
+      parameters: [[':-)', ';~>', ':~>', ';o>', ';>', ':D', ';o(']]
+    }
+  ]
+
+  cases.forEach(({ expected, parameters }) => {
+    it(`should return ${expected} given ${pf(parameters)}`, () => {
+      expect(testSubject(...parameters)).to.equal(expected)
+    })
+  })
+})
---
+++ src/katas/count-smileys.ts
@@ -0,0 +1,23 @@
+/**
+ * @file Katas - countSmileys
+ * @module katas/countSmileys
+ */
+
+/**
+ * Given an array, `arr`, the function returns the total number of smiley faces
+ * in the array.
+ *
+ * @see https://codewars.com/kata/583203e6eb35d7980400002a
+ *
+ * @example countSmileys([':)', ';(', ';}', ':-D']) => 2
+ * @example countSmileys([';D', ':-(', ':-)', ';~)']) => 3
+ * @example countSmileys([';]', ':[', ';*', ':$', ';-D']) => 1
+ *
+ * @param {string[]} arr - Array of strings
+ * @return {number} Number of smiley faces in `arr`
+ */
+const countSmileys = (arr: string[]): number => {
+  return [...(arr.join(' ').match(/((:|;)(-|~)?(\)|D))/g) ?? [])].length
+}
+
+export default countSmileys
