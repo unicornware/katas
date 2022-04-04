f0a721776733f1409989940f298d9634288fc5bf
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-03 23:13:24 -0400

    feat(katas): `humanReadable`
    - https://codewars.com/kata/52685f7382004e774f0001f7

---
+++ src/katas/__tests__/human-readable.spec.ts
@@ -0,0 +1,25 @@
+/**
+ * @file Unit Tests - humanReadable
+ * @module katas/tests/unit/humanReadable
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../human-readable'
+
+describe('unit:katas/humanReadable', () => {
+  interface Case extends TestcaseFn<typeof testSubject> {}
+
+  const cases: Case[] = [
+    { expected: '00:00:00', parameters: [0] },
+    { expected: '00:00:05', parameters: [5] },
+    { expected: '00:01:00', parameters: [60] },
+    { expected: '23:59:59', parameters: [86_399] },
+    { expected: '99:59:59', parameters: [359_999] }
+  ]
+
+  cases.forEach(({ expected, parameters }) => {
+    it(`should return ${pf(expected)} given ${pf(parameters)}`, () => {
+      expect(testSubject(...parameters)).to.equal(expected)
+    })
+  })
+})
