c0e01c3b1a2e8aca0ace46e199fdca6bc2d8d8d5
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-07 18:52:15 -0400

    feat(katas): "Represent array of numbers as ranges"
    - https://codewars.com/kata/58ab002d68ee07c57b000118

---
+++ src/katas/__tests__/array-of-numbers-as-ranges.spec.ts
@@ -0,0 +1,52 @@
+/**
+ * @file Unit Tests - arrayOfNumbersAsRanges
+ * @module katas/tests/unit/arrayOfNumbersAsRanges
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import * as TestSubject from '../array-of-numbers-as-ranges'
+
+describe('unit:katas/arrayOfNumbersAsRanges', () => {
+  describe('.toArray', () => {
+    interface Case extends TestcaseFn<typeof TestSubject['toArray']> {}
+
+    const cases: Case[] = [
+      { expected: [], parameters: [''] },
+      { expected: [2], parameters: ['2'] },
+      { expected: [3, 4, 5, 6, 9], parameters: ['3_6,9'] },
+      {
+        expected: [0, 1, 2, 3, 4, 5, 13, 15, 16, 18, 19, 20],
+        parameters: ['0_5,13,15_16,18_20']
+      }
+    ]
+
+    cases.forEach(({ expected, parameters }) => {
+      it(`should return ${pf(expected)} given ${pf(parameters)}`, () => {
+        expect(TestSubject.toArray(...parameters)).to.deep.equal(expected)
+      })
+    })
+  })
+
+  describe('.toRange', () => {
+    interface Case extends TestcaseFn<typeof TestSubject['toRange']> {}
+
+    const cases: Case[] = [
+      { expected: '', parameters: [[]] },
+      { expected: '2', parameters: [[2]] },
+      { expected: '0,13', parameters: [[0, 13]] },
+      { expected: '-2_2', parameters: [[-2, -1, 0, 1, 2]] },
+      { expected: '0,2_4', parameters: [[0, 2, 3, 4]] },
+      { expected: '3_6,9', parameters: [[3, 4, 5, 6, 9]] },
+      {
+        expected: '0_5,13,15_16,18_20',
+        parameters: [[0, 1, 2, 3, 4, 5, 13, 15, 16, 18, 19, 20]]
+      }
+    ]
+
+    cases.forEach(({ expected, parameters }) => {
+      it(`should return ${pf(expected)} given ${pf(parameters)}`, () => {
+        expect(TestSubject.toRange(...parameters)).to.equal(expected)
+      })
+    })
+  })
+})
