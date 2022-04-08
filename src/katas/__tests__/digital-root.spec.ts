157130d0689b74c36a57e77521e86956c16341c3
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-08 18:30:20 -0400

    feat(katas): `digitalRoot`
    - https://codewars.com/kata/541c8630095125aba6000c00

---
+++ src/katas/__tests__/digital-root.spec.ts
@@ -0,0 +1,26 @@
+/**
+ * @file Unit Tests - digitalRoot
+ * @module katas/tests/unit/digitalRoot
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../digital-root'
+
+describe('unit:katas/digitalRoot', () => {
+  interface Case extends TestcaseFn<typeof testSubject> {}
+
+  const cases: Case[] = [
+    { expected: 3, parameters: [3] },
+    { expected: 7, parameters: [16] },
+    { expected: 6, parameters: [456] },
+    { expected: 6, parameters: [942] },
+    { expected: 6, parameters: [132_189] },
+    { expected: 2, parameters: [493_193] }
+  ]
+
+  cases.forEach(({ expected, parameters }) => {
+    it(`should return ${expected} given ${pf(parameters)}`, () => {
+      expect(testSubject(...parameters)).to.equal(expected)
+    })
+  })
+})
---
+++ src/katas/digital-root.ts
@@ -0,0 +1,43 @@
+/**
+ * @file Katas - digitalRoot
+ * @module katas/digitalRoot
+ */
+
+/**
+ * [Digital root][1] is the _recursive sum of all the digits in a number_.
+ *
+ * Given a number, `n`, the functions returns the sum of all digits in `n`.
+ *
+ * [1]: https://en.wikipedia.org/wiki/Digital_root
+ *
+ * @see https://codewars.com/kata/541c8630095125aba6000c00
+ *
+ * @example digitalRoot(16) // 17
+ * @example digitalRoot(942) // 6
+ * @example digitalRoot(132189) // 6
+ * @example digitalRoot(493193) // 2
+ *
+ * @param {number} n - Number to calculate digital root of
+ * @return {number} Sum of all digits in `n`
+ */
+const digitalRoot = (n: number): number => {
+  // Base case: If n is a single digit number, return n
+  if (n <= 9) return n
+
+  /** @const {number} sum - Sum of all digits in {@link n} */
+  let sum: number = 0
+
+  // Iterate through digits starting from rightmost digit
+  while (n > 0) {
+    // Add rightmost digit
+    sum += Math.floor(n % 10)
+
+    // Move onto next digit
+    n = Math.floor(n / 10)
+  }
+
+  // Return digital root of sum
+  return digitalRoot(sum)
+}
+
+export default digitalRoot
