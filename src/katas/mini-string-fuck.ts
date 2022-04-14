b59116ad9cf24fed27ed32758e9cc42ed0b8d0d0
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-14 16:42:17 -0400

    feat(katas): `miniStringFuck`
    - https://codewars.com/kata/586dd26a69b6fd46dd0000c0

---
+++ src/katas/mini-string-fuck.ts
@@ -0,0 +1,34 @@
+/**
+ * @file Katas - miniStringFuck
+ * @module katas/miniStringFuck
+ * @see https://codewars.com/kata/586dd26a69b6fd46dd0000c0
+ */
+
+/**
+ * [MiniStringFuck][1] interpreter.
+ *
+ * [1]: https://esolangs.org/wiki/MiniStringFuck
+ *
+ * @param {string} code - MiniStringFuck program to execute
+ * @return {string} Program output
+ */
+const miniStringFuck = (code: string): string => {
+  /** @const {number} MAX_MEMORY - Max amount of memory that can be stored */
+  const MAX_MEMORY: number = 255
+
+  /** @var {number} cell - Memory cell */
+  let cell: number = 0
+
+  /** @var {string} output - Program output */
+  let output: string = ''
+
+  // Interpret code
+  for (const command of code) {
+    if (command === '+') cell = cell === MAX_MEMORY ? 0 : cell + 1
+    if (command === '.') output += String.fromCodePoint(cell)
+  }
+
+  return output
+}
+
+export default miniStringFuck
