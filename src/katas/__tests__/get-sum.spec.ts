fe4ff74631b10756dc3ed47fa3700e7fb7ec3b37
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-15 15:03:23 -0400

    feat(katas): `getSum`
    - https://codewars.com/kata/55f2b110f61eb01779000053

---
+++ src/katas/__tests__/get-sum.spec.ts
@@ -0,0 +1,27 @@
+/**
+ * @file Unit Tests - getSum
+ * @module katas/tests/unit/getSum
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../get-sum'
+
+describe('unit:katas/getSum', () => {
+  interface Case extends TestcaseFn<typeof testSubject> {}
+
+  const cases: Case[] = [
+    { expected: -1, parameters: [-1, 0] },
+    { expected: -1, parameters: [0, -1] },
+    { expected: 1, parameters: [0, 1] },
+    { expected: 1, parameters: [1, 0] },
+    { expected: 1, parameters: [1, 1] },
+    { expected: 2, parameters: [-1, 2] },
+    { expected: 3, parameters: [1, 2] }
+  ]
+
+  cases.forEach(({ expected, parameters }) => {
+    it(`should return ${expected} given ${pf(parameters)}`, () => {
+      expect(testSubject(...parameters)).to.equal(expected)
+    })
+  })
+})
