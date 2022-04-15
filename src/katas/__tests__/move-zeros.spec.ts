6da7bddf020cc2c63eafd038437564180dc61e80
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-15 18:42:03 -0400

    feat(katas): `moveZeros`
    - https://codewars.com/kata/52597aa56021e91c93000cb0

---
+++ src/katas/__tests__/move-zeros.spec.ts
@@ -0,0 +1,29 @@
+/**
+ * @file Unit Tests - moveZeros
+ * @module katas/tests/unit/moveZeros
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../move-zeros'
+
+describe('unit:katas/moveZeros', () => {
+  interface Case extends TestcaseFn<typeof testSubject> {}
+
+  const cases: Case[] = [
+    { expected: [1, 1, 0, 0], parameters: [[1, 0, 0, 1]] },
+    {
+      expected: [1, 2, 1, 1, 3, 1, 0, 0, 0, 0],
+      parameters: [[1, 2, 0, 1, 0, 1, 0, 3, 0, 1]]
+    },
+    {
+      expected: [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      parameters: [[9, 0, 9, 0, 1, 0, 2, 0, 1, 0, 1, 0, 3, 0, 1, 0, 9, 0, 9, 0]]
+    }
+  ]
+
+  cases.forEach(({ expected, parameters }) => {
+    it(`should return ${pf(expected)} given ${pf(parameters)}`, () => {
+      expect(testSubject(...parameters)).to.deep.equal(expected)
+    })
+  })
+})
