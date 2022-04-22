444b00fc10d96e5b7634e8f2e53c2f0f24319fee
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-22 13:52:17 -0400

    feat(katas): `divisibleByFive`
    - https://codewars.com/kata/5647c3858d4acbbe550000ad

---
+++ src/katas/__tests__/divisible-by-five.spec.ts
@@ -0,0 +1,38 @@
+/**
+ * @file Unit Tests - divisibleByFive
+ * @module katas/tests/unit/divisibleByFive
+ */
+
+import type { TestcaseFn } from '@tests/interfaces'
+import testSubject from '../divisible-by-five'
+
+describe('unit:katas/divisibleByFive', () => {
+  describe('#test', () => {
+    interface Case extends TestcaseFn<typeof testSubject['test']> {}
+
+    const cases: Case[] = [
+      { expected: false, parameters: [''] },
+      { expected: true, parameters: ['101'] },
+      { expected: true, parameters: ['1010'] },
+      { expected: true, parameters: ['10100'] },
+      { expected: false, parameters: [(21).toString(2)] },
+      { expected: false, parameters: ['1110001'] },
+      { expected: false, parameters: ['10110101'] },
+      { expected: false, parameters: [(328).toString(2)] },
+      { expected: false, parameters: [(5729).toString(2)] },
+      { expected: false, parameters: [(15_392).toString(2)] },
+      { expected: false, parameters: [(19_344).toString(2)] },
+      { expected: false, parameters: [(43_936).toString(2)] },
+      { expected: true, parameters: [(65_020).toString(2)] }
+    ]
+
+    cases.forEach(({ expected, parameters: [string] }) => {
+      const d = Number.parseInt(string, 2)
+      const state = Number.isNaN(d) ? 'empty string' : `${d} as binary number`
+
+      it(`should return ${expected} given ${state}`, () => {
+        expect(testSubject.test(string)).to.equal(expected)
+      })
+    })
+  })
+})
---
+++ src/katas/divisible-by-five.ts
@@ -0,0 +1,40 @@
+/**
+ * @file Katas - divisibleByFive
+ * @module katas/divisibleByFive
+ * @see https://codewars.com/kata/5647c3858d4acbbe550000ad
+ */
+
+/**
+ * Tests if a binary number is divisible by a given number.
+ *
+ * @extends {RegExp}
+ */
+class BinaryDivisibility extends RegExp {
+  /**
+   * Instantiates a new `BinaryDivisibility` regular expression.
+   *
+   * @param {RegExp | string} [pattern='0'] - Divisor
+   */
+  constructor(pattern: RegExp | string = '0') {
+    super(pattern instanceof RegExp ? pattern.source : pattern)
+  }
+
+  /**
+   * Checks if `string`, a binary number, is divisible by `+this.source`.
+   *
+   * @param {string} string - Binary number to test
+   * @return {boolean} `true` if `string` is divisible, `false` otherwise
+   */
+  test(string: string): boolean {
+    return +Number.parseInt(string, 2) % +this.source === 0
+  }
+}
+
+/**
+ * Checks if a binary number is divisible by `5`.
+ *
+ * @const {BinaryDivisibility} divisibleByFive
+ */
+const divisibleByFive = new BinaryDivisibility('5')
+
+export default divisibleByFive
