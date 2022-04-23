2c2da8eb2b2207597bf2019c7bc847c75aa4e0ed
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-22 21:21:30 -0400

    feat(katas): `countSheep`
    - https://codewars.com/kata/54edbc7200b811e956000556

---
+++ src/katas/count-sheep.ts
@@ -0,0 +1,21 @@
+/**
+ * @file Katas - countSheep
+ * @module katas/countSheep
+ * @see https://codewars.com/kata/54edbc7200b811e956000556
+ */
+
+/**
+ * Given an array where some "sheep" may be missing from their place, `arr`, the
+ * function returns the total number of "sheep" present in the array.
+ *
+ * @example
+ *  countSheep([true, false, null, undefined, true, true]) // 3
+ *
+ * @param {(boolean | null | undefined)[]} arr - Sheep array
+ * @return {number} Total number of sheep in `arr`
+ */
+const countSheep = (arr: (boolean | null | undefined)[]): number => {
+  return arr.reduce((acc, curr) => acc + (curr ? 1 : 0), 0)
+}
+
+export default countSheep
