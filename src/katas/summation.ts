0b28708044c24225e7d3664a55a9a97c8f175f55
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-04-22 20:30:10 -0400

    feat(katas): `summation`
    - https://codewars.com/kata/55d24f55d7dd296eb9000030

---
+++ src/katas/summation.ts
@@ -0,0 +1,21 @@
+/**
+ * @file Katas - summation
+ * @module katas/summation
+ * @see https://codewars.com/kata/55d24f55d7dd296eb9000030
+ */
+
+/**
+ * Given a positive integer, `n`, the function returns the summation of every
+ * number within the range `[1, n]`.
+ *
+ * @example
+ *  summation(2) // 3
+ * @example
+ *  summation(8) // 36
+ *
+ * @param {number} n - Positive integer greater than `0`
+ * @return {number} Summation of every number from 1 to `n`
+ */
+const summation = (n: number): number => n === 1 ? n : n + summation(n - 1)
+
+export default summation
